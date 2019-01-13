b20ae2588bdb4dcfe5d95c18295d471b53391b20
Daisy Dinh <daisydodinh@gmail.com>
2019-01-12 22:59:07 -0600

    Updates

---
+++ precache-manifest.28d57afb7f01581b5963980d99f51b42.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f2e24cfc8e83b96db2c",
+    "url": "/Stamp-clicky-game/static/css/main.65d538c6.chunk.css"
+  },
+  {
+    "revision": "1f2e24cfc8e83b96db2c",
+    "url": "/Stamp-clicky-game/static/js/main.1f2e24cf.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/Stamp-clicky-game/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "c94edb33336f72b3de93",
+    "url": "/Stamp-clicky-game/static/js/runtime~main.c94edb33.js"
+  },
+  {
+    "revision": "535cecf7b077745b18b4945e6de10514",
+    "url": "/Stamp-clicky-game/index.html"
+  }
+];
